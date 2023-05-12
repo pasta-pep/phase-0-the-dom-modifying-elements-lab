0796efb19117c490f871a8f8d0623b295ab593e1
joseph <jcw.pep@gmail.com>
2023-05-12 15:41:50 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+const yourName = "j"
+newHeader.textContent = "j is the champion";
+document.body.append(newHeader);
+
+
